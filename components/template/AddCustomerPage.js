4084158d0915c9c315f19965a8a27286f0b34e6a
miss-f-g <fatemeghafari98@gmail.com>
2023-02-20 11:01:29 +0330

    ep138

--- components/template/AddCustomerPage.js
+++ components/template/AddCustomerPage.js
@@ -1,3 +1,4 @@
+import { useRouter } from "next/router";
 import React, { useState } from "react";
 import Form from "../module/Form";
 
@@ -13,8 +14,33 @@ const AddCustomerPage = () => {
     product: [],
   });
 
-  const cancelHandler =()=>{}
-  const saveHandler =()=>{}
+  const router = useRouter();
+
+  const saveHandler = async () => {
+    const res = await fetch("/api/customer", {
+      method: "POST",
+      body: JSON.stringify({ data: form }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    const data = await res.json();
+    console.log(data);
+    if (data.status === "success") router.push("/");
+  };
+
+  const cancelHandler = () => {
+    setForm({
+      name: "",
+      lastName: "",
+      email: "",
+      phone: "",
+      address: "",
+      postalCode: "",
+      date: "",
+      product: [],
+    });
+    router.push("/");
+  };
 
   return (
     <div className="customer-page">
@@ -22,8 +48,12 @@ const AddCustomerPage = () => {
       <Form form={form} setForm={setForm} />
 
       <div className="customer-page__buttons">
-        <button className="first" onClick={cancelHandler} >Cancel</button>
-        <button className="second" onClick={saveHandler}>Save</button>
+        <button className="first" onClick={cancelHandler}>
+          Cancel
+        </button>
+        <button className="second" onClick={saveHandler}>
+          Save
+        </button>
       </div>
     </div>
   );
