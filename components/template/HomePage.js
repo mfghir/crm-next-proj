b5b80f1aff33dbfd60da0d4edf8b1ff2d437afd7
miss-f-g <fatemeghafari98@gmail.com>
2023-02-20 14:28:00 +0330

    ep144

---
+++ components/template/HomePage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HomePage = ({customers}) => {
+  return (
+    <div>HomePage</div>
+  )
+}
+
+export default HomePage
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,23 @@
-export default function Home() {
-  return <h1>crm</h1>;
+import HomePage from "@/components/template/HomePage";
+import Customer from "@/models/Customer";
+import connectDB from "@/utils/connectDB";
+
+function Index({ customers }) {
+  console.log(customers);
+  return <HomePage customers={customers} />;
+}
+export default Index;
+
+export async function getStaticProps() {
+  try {
+    await connectDB();
+    const customers = await Customer.find();
+    return {
+      props: {
+        customers: JSON.parse(JSON.stringify(customers)),
+      },
+    };
+  } catch (err) {
+    notFound: true;
+  }
 }
