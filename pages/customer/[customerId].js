7776dd15c37deff09045116c0899beef408f577b
miss-f-g <fatemeghafari98@gmail.com>
2023-02-22 12:19:55 +0330

    ep151

---
+++ pages/customer/[customerId].js
@@ -0,0 +1,26 @@
+import CustomerDetailPage from "@/components/template/CustomerDetailPage";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+
+const Index = () => {
+  const [data, setData] = useState(null);
+  const router = useRouter();
+  const {
+    query: { customerId },
+    isReady,
+  } = router;
+
+  useEffect(() => {
+    if (isReady) {
+      fetch(`/api/customer/${customerId}`)
+        .then((res) => res.json())
+        .then((data) => setData(data));
+    }
+  }, [isReady]);
+
+  if (data) return <CustomerDetailPage data={data} />;
+
+  return <div>Index</div>;
+};
+
+export default Index;
