2dac5fc995d86de7fc8cb4862e79939befd5e2e7
miss-f-g <fatemeghafari98@gmail.com>
2023-02-22 11:07:41 +0330

    ep149

---
+++ components/template/CustomerEditPage.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const CustomerEditPage = ({ data, id }) => {
+  console.log({ data, id });
+  return <div>CustomerEditPage</div>;
+};
+
+export default CustomerEditPage;
